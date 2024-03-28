24f5150a08e0ad0db4d90b65b8638383830f1dec
Seth Blanchard <blanchard.seth@gmail.com>
2024-03-27 17:35:36 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const books = new Map<string, any>();
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
